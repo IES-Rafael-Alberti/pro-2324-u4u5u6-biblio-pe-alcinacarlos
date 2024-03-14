3dc8c03cfe96e59fe7d4bfb8bceb8d2ea1007c06
alcinacarlos <calcrom0607@g.educaand.es>
2024-03-14 20:54:12 +0100

    ejercicio 5 no esta terminado

--- src/main/kotlin/GestorElementos.kt
+++ src/main/kotlin/GestorElementos.kt
@@ -1,2 +1,17 @@
-class GestorElementos<T>(private val procesador: ElementoBiblioteca<T>) {
+class GestorElementos<T : ElementoBiblioteca<T>> {
+
+    private val elementos: MutableList<T> = mutableListOf()
+    fun agregar(elemento: T) {
+        elementos.add(elemento)
+    }
+    fun eliminar(id: String) {
+        elementos.removeIf { it.id == id }
+    }
+
+    fun obtenerTodos(): List<T> {
+        return elementos.toList()
+    }
+    fun filtrarPorCriterio(criterio: (T) -> Boolean): List<T> {
+        return elementos.filter(criterio)
+    }
 }
